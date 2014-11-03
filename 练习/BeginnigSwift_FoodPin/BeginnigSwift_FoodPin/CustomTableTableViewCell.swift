c10dd3db634bb19c2168a6e9d6c9e04dafe95599
Jack <anglexxyy@qq.com>
2014-11-03 19:02:57 +0800

    做Beginning Programming with Swift的练习

---
+++ 练习/BeginnigSwift_FoodPin/BeginnigSwift_FoodPin/CustomTableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableTableViewCell.swift
+//  BeginnigSwift_FoodPin
+//
+//  Created by NKO on 14-11-3.
+//  Copyright (c) 2014年 NKO. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
